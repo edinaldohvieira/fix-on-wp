32fb4e9572017b2946346032fb8b38a464a6a828
edinaldohvieira <edinaldohvieira@gmail.com>
2020-02-22 07:15:52 -0300

    versão 0.1.1 start 5

--- fix-on-wp.php
+++ fix-on-wp.php
@@ -7,7 +7,7 @@
  * Author URI:      https://github.com/edinaldohvieira
  * Text Domain:     fix-on-wp
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.1.1
  * Referencia:      fix158235
  *
  * @package         Fix_On_Wp
